cef0cb934c581673b780f9b459cdc346390c18a6
Ilija Poznic <ikapoz@gmail.com>
2022-03-11 12:54:46 +0100

    Added check box to ExitDialog and CA on install and onremove

---
+++ WinFAppInstaller/WinFApp.CA/OnIntall.cs
@@ -0,0 +1,73 @@
+﻿using IWshRuntimeLibrary;
+using Microsoft.Deployment.WindowsInstaller;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace WinFApp.CA
+{
+    public class OnIntall
+    {
+        [CustomAction]
+        public static ActionResult CheckRunAppOnSystemStartupAndLauchApplication(Session session)
+        {
+            var result = ActionResult.Success;
+            var actionName = $"WinFApp.CA.{MethodBase.GetCurrentMethod().Name}";
+            session.Log($"started: {actionName}");
+            try
+            {
+                var runOnWinStartupValue = session["FA_RUNAPPONSYSTEMSTARTUPCHECKBOX"];
+                var launchAppValue = session["FA_LAUNCHAPPCHECKBOX"];
+                var installDir = session["INSTALLDIR"];
+                var exeName = "WinFApp.exe";
+                var exePath = System.IO.Path.Combine(session["INSTALLDIR"], exeName);
+                var runOnWinStartup = session["FA_RUNAPPONSYSTEMSTARTUPCHECKBOX"].Equals("1");
+                var launchApp = session["FA_LAUNCHAPPCHECKBOX"].Equals("1");
+                if (launchApp)
+                {
+                    var startInfo = new ProcessStartInfo()
+                    {
+                        FileName = "WinFApp.exe",
+                        WorkingDirectory = installDir,
+                    };
+                    var process = Process.Start(startInfo);
+                    process.WaitForInputIdle();
+                }
+
+                if (runOnWinStartup)
+                {
+                    var productName = session["ProductName"];
+                    WshShellClass wshShell = new WshShellClass();
+                    IWshRuntimeLibrary.IWshShortcut shortcut;
+                    string startUpFolderPath =
+                      Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+
+                    // Create the shortcut
+                    shortcut =
+                      (IWshRuntimeLibrary.IWshShortcut)wshShell.CreateShortcut(
+                        startUpFolderPath + "\\" +
+                        productName + ".lnk");
+
+                    shortcut.TargetPath = exePath;
+                    shortcut.WorkingDirectory = installDir;
+                    shortcut.Description = $"Launch {productName}";
+                    shortcut.Save();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                session.Log($"{actionName}|{ex}");
+                result = ActionResult.Failure;
+            }
+
+            session.Log($"ended: {actionName}");
+            return result;
+
+        }
+    }
+}
---
+++ WinFAppInstaller/WinFApp.CA/OnRemove.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace WinFApp.CA
+{
+    public class OnRemove
+    {
+        [CustomAction]
+        public static ActionResult RemoveAutorunLink(Session session)
+        {
+            var result = ActionResult.Success;
+            var actionName = $"WinFApp.CA.{MethodBase.GetCurrentMethod().Name}";
+            session.Log($"started: {actionName}");
+            try
+            {
+                string startUpFolderPath =
+                   Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+                var productName = session["ProductName"];
+                var fileName = Path.Combine(startUpFolderPath, $"{productName}.lnk");
+                if (File.Exists(fileName))
+                    File.Delete(fileName);
+            }
+            catch (Exception ex)
+            {
+                session.Log($"{actionName}|{ex}");
+                result = ActionResult.Failure;
+            }
+
+            session.Log($"ended: {actionName}");
+            return result;
+        }
+    }
+}
